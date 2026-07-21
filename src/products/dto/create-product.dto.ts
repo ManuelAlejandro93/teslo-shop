641003df19e1375f9cb7175d0e44c567d458b238
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2026-07-21 17:53:10 -0500

    Product Module Building - DTO creation - createDTO...Done

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,68 @@
-export class CreateProductDto {}
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Min,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  //description
+  @IsOptional()
+  @IsString()
+  @MinLength(10)
+  description: string;
+
+  //images
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  @ArrayMinSize(1)
+  images?: string[];
+
+  //stock
+  @IsNumber()
+  @IsPositive()
+  stock: number;
+
+  //price
+  @IsNumber()
+  @Min(0)
+  price: number;
+
+  //sizes
+  @IsArray()
+  @IsString({ each: true })
+  @ArrayMinSize(1)
+  sizes: string[];
+
+  //slug
+  @IsString()
+  @MinLength(10)
+  slug: string;
+
+  //type
+  @IsString()
+  @MinLength(3)
+  type: string;
+
+  //tags
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  @ArrayMinSize(1)
+  tags: string[];
+
+  //title
+  @IsString()
+  @MinLength(3)
+  title: string;
+
+  //gender
+  @IsString()
+  @MinLength(3)
+  gender: string;
+}
