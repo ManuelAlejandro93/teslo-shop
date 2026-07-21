c2c6db2f3693ce48a761847d202595df0e31c5f5
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2026-07-21 17:04:04 -0500

    Product Module Building - Entity Creation - aparently ok

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -8,7 +8,31 @@ export class Product {
   // description
   @Column({ type: 'text', nullable: true })
   description: string;
+  //images
+  @Column({ type: 'text', array: true, nullable: true })
+  images: string[];
+  //stock
+  @Column({ type: 'numeric', default: 0, nullable: false })
+  stock: number;
   // price
   @Column({ type: 'numeric', default: 0, nullable: false })
-  price: string;
+  price: number;
+  //sizes
+  @Column({ type: 'text', array: true, nullable: false })
+  sizes: string[];
+  @Column({ type: 'text', nullable: false, unique: true })
+  //slug
+  slug: string;
+  //type
+  @Column({ type: 'text', nullable: false })
+  type: string;
+  //tags
+  @Column({ type: 'text', array: true, default: [], nullable: true })
+  tags: string[];
+  //title
+  @Column({ type: 'text', nullable: false })
+  title: string[];
+  //gender
+  @Column({ type: 'text', nullable: false })
+  gender: string[];
 }
